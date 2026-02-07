6585b312ed8bb3141ee66ae20bd529b0e7668765
Binovold <binovold@gmail.com>
2026-02-07 13:52:27 -0500

    Added an option to choose the amount of players

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const paragraph = document.querySelector('p');
 const word = document.querySelector('h1');
 let count = 0;
 let playerCount = 0;
-let imposter = Math.floor(Math.random() * 8) + 1;
+let imposter;
+
+const playersDisplay = document.querySelector('#players h3');
+let players = parseInt(playersDisplay.innerHTML);
+const minusBtn = document.querySelector('#players button:first-of-type');
+const plusBtn = document.querySelector('#players button:last-child');
 
 fetch('items.json')
     .then(response => response.json())
@@ -14,10 +19,21 @@ fetch('items.json')
     });
 
 document.querySelector('.button').onclick = () => {
+    ChooseImposter();
+    document.querySelector('.lower').style.display = 'none';
+
     if (button.innerHTML === 'Done') {
         return;
     }
 
+    ShowPage();
+
+    if (playerCount === players && count % 2 === 0) {
+    button.innerHTML = 'Done';
+    }
+};
+
+function ShowPage() {
     if (count % 2 === 0 || count === 0) {
         playerCount++;
         button.innerHTML = 'Show My Word';
@@ -34,8 +50,24 @@ document.querySelector('.button').onclick = () => {
     }
     title.innerHTML = `Player ${playerCount}`;
     count++;
+}
 
-    if (count === 16) {
-    button.innerHTML = 'Done';
+plusBtn.addEventListener('click', () => {
+    if (players < 12) {
+        players++;
+        playersDisplay.textContent = players;
     }
-};
+})
+
+minusBtn.addEventListener('click', () => {
+    if (players > 3) {
+        players--;
+        playersDisplay.textContent = players;
+    }
+});
+
+function ChooseImposter() {
+    if (count === 0) {
+        imposter = Math.floor(Math.random() * players) + 1;
+    }
+}
